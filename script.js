940e888c576b0935cc717ffd739faa263c26b028
JKLock <joeri.lock@live.nl>
2020-08-03 14:54:53 +0200

    Also removes stickers if removeStickers == 1

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
 people = [
-	"person1",
-	"person2"
+  "Ruben Synergia",
+"Maarten Robert Wulp, van der Wulp, van der",
+"Seraphin Rodriguez Rodriguez"
 ]
 
-function removePeople(){
-	people.forEach(function(person){
-		elements = getAllMessageFrom(person);
-		elements.forEach(element => element.remove());
-	}) 
+removeStickers = 1;
+
+function removeStuff(){
+  people.forEach(function(person){
+    elements = getAllMessageFrom(person);
+    elements.forEach(element => element.remove());
+  }) 
+  if(removeStickers){
+    removeAllStickers()
+  }
+
 }
 
+function removeAllStickers(){
+  document.querySelectorAll('._2et95.urabC').forEach(sticker => sticker.remove())
+}
 
 function getAllMessageFrom(person){
-	messages = document.querySelectorAll('.FMlAw.FdF4z._3Whw5');
-	elements = [];
-	for (i = 0; i < messages.length; i++) {
+  messages = document.querySelectorAll('.FMlAw.FdF4z._3Whw5');
+  elements = [];
+  for (i = 0; i < messages.length; i++) {
       if(messages[i].innerText == person){
-      	wholeMsg = messages[i].closest(".message-in")
-      	elements.push(wholeMsg);
-      	while(wholeMsg.nextElementSibling && !wholeMsg.nextElementSibling.lastElementChild.classList.contains('_1dvTE')){
-      		elements.push(wholeMsg.nextElementSibling);
-      		wholeMsg = wholeMsg.nextElementSibling;
-      	}
+        wholeMsg = messages[i].closest(".message-in")
+        elements.push(wholeMsg);
+        while(wholeMsg.nextElementSibling && !wholeMsg.nextElementSibling.lastElementChild.classList.contains('_1dvTE')){
+          elements.push(wholeMsg.nextElementSibling);
+          wholeMsg = wholeMsg.nextElementSibling;
+        }
       }
     }
     return elements;
 }
-setInterval(removePeople,1000);
+setInterval(removeStuff,1000);
